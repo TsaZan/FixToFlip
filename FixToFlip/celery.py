dfc011288aff698e612759399616ecc8d9377892
TsaZan <87821236+TsaZan@users.noreply.github.com>
2024-11-25 22:57:05 +0200

    Added: Celery With Redis Broker Added: task.py for testing

---
+++ FixToFlip/celery.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FixToFlip.settings')
+
+app = Celery('FixToFlip')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks(['FixToFlip.common'])
+
