92e0cfdb383ff74870f3ae44823cbb8f6616ac9e
TsaZan <87821236+TsaZan@users.noreply.github.com>
2024-11-27 14:37:09 +0200

    Fixed: Offer Status Error

--- FixToFlip/offers/models.py
+++ FixToFlip/offers/models.py
@@ -13,20 +13,20 @@ class Offer(models.Model):
 
     description = models.TextField()
 
-    image = CloudinaryField('image', blank=True, null=True,)
+    image = CloudinaryField('image', blank=True, null=True, )
 
-    video = CloudinaryField('video', resource_type='video', blank=True, null=True,)
+    video = CloudinaryField('video', resource_type='video', blank=True, null=True, )
 
-    price = MoneyField(max_digits=10, decimal_places=2, default=Money(0, 'EUR'), blank=True, null=True,)
+    price = MoneyField(max_digits=10, decimal_places=2, default=Money(0, 'EUR'), blank=True, null=True, )
 
-    offer_validity = models.DateField( blank=True, null=True,)
+    offer_validity = models.DateField(blank=True, null=True, )
 
-    brokerage_commission = MoneyField(max_digits=8, decimal_places=2, default=Money(0, 'EUR'), blank=True, null=True,)
+    brokerage_commission = MoneyField(max_digits=8, decimal_places=2, default=Money(0, 'EUR'), blank=True, null=True, )
 
     offer_status = models.CharField(
-        max_length=10,
+        max_length=15,
         choices=OfferStatusChoices.choices,
-        blank = True, null = True,
+        blank=True, null=True,
     )
 
     created_at = models.DateTimeField(
@@ -37,9 +37,9 @@ class Offer(models.Model):
         auto_now=True, blank=True, null=True,
     )
 
-    rooms = models.PositiveIntegerField( blank=True, null=True,)
+    rooms = models.PositiveIntegerField(blank=True, null=True, )
 
-    property_size = models.PositiveIntegerField(blank=True, null=True,)
+    property_size = models.PositiveIntegerField(blank=True, null=True, )
 
     def __str__(self):
         return self.title
