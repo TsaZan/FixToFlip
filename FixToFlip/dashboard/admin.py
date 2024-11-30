8f1b038caeb0b749d354274894dfd6c1647f78d1
TsaZan <87821236+TsaZan@users.noreply.github.com>
2024-11-30 11:51:31 +0200

    Updated: Admin Function Fixed: Workflow

--- FixToFlip/dashboard/admin.py
+++ FixToFlip/dashboard/admin.py
@@ -1,22 +1,18 @@
 from django.contrib import admin
-from allauth.account.admin import EmailAddressAdmin
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.models import SocialApp, SocialToken, SocialAccount
 from cities_light.admin import SubRegion, City, Country, Region
 from django.contrib.sites.models import Site
 from djmoney.contrib.exchange.admin import Rate
-from allauth.socialaccount.admin import SocialAccountAdmin, SocialTokenAdmin, SocialAppAdmin
 
 admin.site.unregister(Country)
 admin.site.unregister(City)
 admin.site.unregister(Region)
 admin.site.unregister(SubRegion)
-admin.site.unregister(Site)
 admin.site.unregister(Rate)
 admin.site.unregister(SocialApp)
 admin.site.unregister(SocialToken)
 admin.site.unregister(SocialAccount)
-admin.site.unregister(EmailAddress)
 
 
 
