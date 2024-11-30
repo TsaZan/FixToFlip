089005a4a2264f4f2af07003d3df1c2fc4dd24a6
TsaZan <87821236+TsaZan@users.noreply.github.com>
2024-11-30 16:44:48 +0200

    Updated: Profile Forms And Model Updated: BaseAccount Model Added: Only Letters Validator Added: Custom CSS for errors

--- FixToFlip/validators.py
+++ FixToFlip/validators.py
@@ -1,5 +1,4 @@
 from django.core.exceptions import ValidationError
-from django.db import models
 import datetime
 
 
@@ -14,3 +13,13 @@ def get_current_date():
 def offer_image_max_size_validator(value):
     if value.size > 10 * 1024 * 1024:
         raise ValidationError(f'Image size must be less than 10MB. Your file is {(value.size / 1024 / 1024):.0f}MB')
+
+
+class OnlyLettersValidator:
+    def __init__(self, message=None):
+        self.message = message or 'Only letters are allowed.'
+
+    def __call__(self, value):
+        for char in value:
+            if not char.isalpha():
+                raise ValidationError(self.message)
