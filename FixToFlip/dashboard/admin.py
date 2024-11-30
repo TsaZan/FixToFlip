487846a46ef29ebf9f3942416ab92f36809fdb50
TsaZan <87821236+TsaZan@users.noreply.github.com>
2024-11-30 11:58:35 +0200

    Updated: Admin Function

--- FixToFlip/dashboard/admin.py
+++ FixToFlip/dashboard/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
+from allauth.account.admin import EmailAddressAdmin
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.models import SocialApp, SocialToken, SocialAccount
 from cities_light.admin import SubRegion, City, Country, Region
 from django.contrib.sites.models import Site
 from djmoney.contrib.exchange.admin import Rate
+from allauth.socialaccount.admin import SocialAccountAdmin, SocialTokenAdmin, SocialAppAdmin
 
 admin.site.unregister(Country)
 admin.site.unregister(City)
